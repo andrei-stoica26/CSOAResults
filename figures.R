91acc4282cbfab6d3e10e6a970abd5c7cc65e397
andrei-stoica26 <andreistoica@foxmail.com>
2025-11-10 23:11:59 +0800

    Added more figures

--- figures.R
+++ figures.R
@@ -69,6 +69,10 @@ umapBlood <- umapPlots2(seuratBlood, smrBlood, 'funct', 'cell.killing',
                        labelSize)
 devPlot(octoPlot2, umapPanc, umapLung, umapBreast, umapBlood, 1, 2)
 
+seuratPancShuffle <- qread('seuratPancShuffle.qs')
+seuratLungShuffle <- qread('seuratLungShuffle.qs')
+seuratBreastShuffle <- qread('seuratBreastShuffle.qs')
+seuratBloodShuffle <- qread('seuratBloodShuffle.qs')
 
 smrPancShuffle <- qread('smrPancShuffle.qs')
 smrLungShuffle <- qread('smrLungShuffle.qs')
@@ -85,4 +89,40 @@ for (pair in list(c(8, 6), c(9, 10), c(19, 15), c(20, 21)))
             plotsBreastShuffle, plotsBloodShuffle,
             pair[1], pair[2])
 
+segWidth <- 0.1
+labelSize <- 1
+pointSize <- 0.05
+labelSegWidth <- 0.1
+maxOverlaps <- 10
+mdsPancShuffle <- mdsPlots(seuratPancShuffle, smrPancShuffle, pointSize=pointSize,
+                    labelSize=labelSize, segWidth=segWidth,
+                    labelSegWidth=labelSegWidth, maxOverlaps=maxOverlaps)$aggregate
+mdsLungShuffle <- mdsPlots(seuratLungShuffle, smrLungShuffle, pointSize=pointSize,
+                    labelSize=labelSize, segWidth=segWidth,
+                    labelSegWidth=labelSegWidth, maxOverlaps=maxOverlaps)$aggregate
+mdsBreastShuffle <- mdsPlots(seuratBreastShuffle, smrBreastShuffle, pointSize=pointSize,
+                      labelSize=labelSize, segWidth=segWidth,
+                      labelSegWidth=labelSegWidth, maxOverlaps=maxOverlaps)$aggregate
+mdsBloodShuffle <- mdsPlots(seuratBloodShuffle, smrBloodShuffle, pointSize=pointSize,
+                     labelSize=labelSize, segWidth=segWidth,
+                     labelSegWidth=labelSegWidth, maxOverlaps=maxOverlaps)$aggregate
 
+jacPancShuffle <- predJaccardPlots(smrPancShuffle$predictions,
+                            labelSize=labelSize,
+                            legendTitle='Jaccard')$aggregate
+jacLungShuffle <- predJaccardPlots(smrLungShuffle$predictions,
+                            labelSize=labelSize,
+                            legendTitle='Jaccard')$aggregate
+jacBreastShuffle <- predJaccardPlots(smrBreastShuffle$predictions,
+                              labelSize=labelSize,
+                              legendTitle='Jaccard')$aggregate
+jacBloodShuffle <- predJaccardPlots(smrBloodShuffle$predictions,
+                             labelSize=labelSize,
+                             legendTitle='Jaccard')$aggregate
+
+devPlot(octoPlot,
+        list(mdsPancShuffle, jacPancShuffle),
+        list(mdsLungShuffle, jacLungShuffle),
+        list(mdsBreastShuffle, jacBreastShuffle),
+        list(mdsBloodShuffle, jacBloodShuffle),
+        1, 2)
