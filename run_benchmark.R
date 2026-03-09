fc9d2142b37213657053c1f15233d285dd7693c7
andrei-stoica26 <andreistoica@foxmail.com>
2026-03-09 11:19:37 +0800

    Edited the benchmark function

--- run_benchmark.R
+++ run_benchmark.R
@@ -1,25 +1,25 @@
 benchmark <- function(){
-    seuratPanc <- qread('seuratPancGSA.qs')
-    seuratLung <- qread('seuratLungGSA.qs')
-    seuratBreast <- qread('seuratBreastGSA.qs')
-    seuratBlood <- qread('seuratBloodGSA.qs')
+    seuratPanc <- qs_read('seuratPancGSA.qs2')
+    seuratLung <- qs_read('seuratLungGSA.qs2')
+    seuratBreast <- qs_read('seuratBreastGSA.qs2')
+    seuratBlood <- qs_read('seuratBloodGSA.qs2')
 
-    geneSetsPanc <- qread('geneSetsPanc.qs')
-    geneSetsLung <- qread('geneSetsLung.qs')
-    geneSetsBreast <- qread('geneSetsBreast.qs')
-    geneSetsBlood <- qread('geneSetsBlood.qs')
+    geneSetsPanc <- qs_read('geneSetsPanc.qs2')
+    geneSetsLung <- qs_read('geneSetsLung.qs2')
+    geneSetsBreast <- qs_read('geneSetsBreast.qs2')
+    geneSetsBlood <- qs_read('geneSetsBlood.qs2')
 
     gsaMethods <- supportedMethods()
 
     smrPanc <- runBenchmark(seuratPanc, 'label', geneSetsPanc, gsaMethods)
-    qsave(smrPanc, 'smrPanc.qs')
+    qs_save(smrPanc, 'smrPanc.qs2')
 
     smrLung <- runBenchmark(seuratLung, 'celltype', geneSetsLung, gsaMethods)
-    qsave(smrLung, 'smrLung.qs')
+    qs_save(smrLung, 'smrLung.qs2')
 
     smrBreast <- runBenchmark(seuratBreast, 'funct', geneSetsBreast, gsaMethods)
-    qsave(smrBreast, 'smrBreast.qs')
+    qs_save(smrBreast, 'smrBreast.qs2')
 
     smrBlood <- runBenchmark(seuratBlood, 'funct', geneSetsBlood, gsaMethods)
-    qsave(smrBlood, 'smrBlood.qs')
+    qs_save(smrBlood, 'smrBlood.qs2')
 }
