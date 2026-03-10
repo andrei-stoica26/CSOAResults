5bbcc52dd336625ad4742dee2ebe2a44143243d1
andrei-stoica26 <andreistoica@foxmail.com>
2026-03-10 16:48:47 +0800

    Removed obsolete files

--- visualization_abstract.R
+++ visualization_abstract.R
@@ -1,8 +1,7 @@
-geneCellCountPlot <- function(df, yCol, fillColor, title){
-    p <- ggplot(df, aes(x = Gene, y = {{yCol}})) +
+geneCellCountPlot <- function(df, fillColor, title){
+    p <- ggplot(df, aes(x = Gene, y = nCells)) +
         geom_bar(stat = "identity", fill = fillColor, width = 0.6) +
         ylab('Number of cells') +
-        ylim(0, 40000) +
         xlab("Signature gene")
     p <- editAxes(p)
     p <- centerTitle(p, title)
@@ -11,13 +10,10 @@ geneCellCountPlot <- function(df, yCol, fillColor, title){
 
 eulerPlot <- function(geneSets,
                       title,
-                      fills = c('blue', 'yellow', 'red', 'purple', 'green',
-                                'orange', 'lavender', 'plum')){
+                      fills = c('red','yellow')){
     eulerObj <- euler(geneSets)
     p <- as.ggplot(plot(eulerObj, fills=fills,
-                   labels=list(
-                       font=rep(1, length(geneSets)),
-                       cex=rep(0.4, length(geneSets)))
+                        labels=NULL
     ))
     p <- centerTitle(p, title)
     p <- p + theme(plot.title=element_text(size=ABS_TEXT_SIZE,
@@ -37,8 +33,22 @@ prerankPlot <- function(df, title=NULL){
     return(p)
 }
 
+prerankPlot2 <- function(df, title=NULL){
+    p <- ggplot(df, aes(x = overlap, y = rank, fill = rankType)) +
+        geom_col(position = "dodge") +
+        labs(x = "Overlap", y = "Rank", fill = "Rank type") +
+        scale_fill_manual(values = wes_palette('Darjeeling1')[c(2, 1, 3)],
+                          labels = c('p-value rank', 'Ratio rank',
+                                     'Aggregate rank'))
+    p <- editAxes(p)
+    p <- centerTitle(p, title)
+    p <- editLegend(p)
+    return(p)
+}
+
 rankScorePlot <- function(df, title=NULL){
-    p <- ggplot(df, aes(x, y)) + geom_line(color = 'mediumpurple4') +
+    p <- ggplot(df, aes(x = rank, y = score)) +
+        geom_line(color = 'mediumpurple4') +
         geom_point(color = 'red', size = 1) +
         labs(x = 'Overlap rank', y = 'Overlap score')
     p <- editAxes2(p)
@@ -46,27 +56,4 @@ rankScorePlot <- function(df, title=NULL){
     return(p)
 }
 
-geneExpPlot <- function(df, title, ylab = 'Gene', colorIndex = 1){
-    colorSchemes <- list(wes_palette('IsleofDogs1')[c(5, 1)],
-                         wes_palette('Royal1')[c(3, 2)],
-                         wes_palette('AsteroidCity2')[c(3, 5)]
-    )
-    p <- ggplot(df, aes(x = column, y = factor(row), fill = value)) +
-        geom_tile() +
-        scale_fill_gradientn(colors = colorSchemes[[colorIndex]], breaks = c(0, 0.5, 1)) +
-        labs(x = 'Cell', y = ylab)
-    p <- editAxes(p)
-    p <- centerTitle(p, title)
-    p <- editLegend(p)
-    return(p)
-}
 
-scoreFeaturePlot <- function(seuratObj, feature, title){
-    p <- FeaturePlot(seuratObj, feature)
-    p <- editAxes(p)
-    p <- centerTitle(p, title, face='plain')
-    p <- editLegend(p)
-    p <- p + scale_color_gradientn(colors = wes_palette('Royal1')[c(3, 2)],
-                                   breaks = c(0, 0.5, 1))
-    return(p)
-}
