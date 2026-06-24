0422a3637151054763a317eab498b41a4710357f
andrei-stoica26 <andreistoica@foxmail.com>
2026-06-24 16:17:45 +0800

    Added arrowDF and pointDF

--- new_first_figure.R
+++ new_first_figure.R
@@ -16,11 +16,27 @@ ellipseDF <- data.frame(x0 = c(6, 6, 6),
                         color = c('purple', 'blue', 'red'),
                         label = c('Cell set n', 'Cell set 2', 'Cell set N'))
 
-ggplot() + theme_void() +
+arrowDF <- data.frame(x=c(3, 3, 3),
+                      y=c(1, 5, 8),
+                      xend=c(4, 4, 4),
+                      yend=c(1, 5, 8))
+
+pointDF <- data.frame(x=c(3.1, 3.5, 3.9),
+                      y=c(3, 3, 3))
+
+lnWidth <- 0.8
+
+ggplot() + theme_classic() +
     geom_rect(data=rectDF,
               aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
-              fill='white', color=rectDF$color) +
+              fill='white', color=rectDF$color, linewidth=lnWidth) +
     geom_text(data=rectDF, aes(x=xmin + 1.5, y=ymin + 1, label=label)) +
     geom_ellipse(data=ellipseDF, aes(x0=x0, y0=y0, a=a, b=b, angle=angle),
-                 fill='white') +
-    geom_text(data=ellipseDF, aes(x=x0, y=y0, label=label))
+                 fill='white', linewidth=lnWidth) +
+    geom_text(data=ellipseDF, aes(x=x0, y=y0, label=label)) +
+    geom_segment(data=arrowDF, aes(x=x, y=y, xend=xend), arrow=arrow(),
+                 linewidth=lnWidth) +
+    geom_point(data=pointDF, aes(x=x, y=y))
+
+
+
