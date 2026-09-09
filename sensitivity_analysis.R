735e93c6a3855467513e7510f26ab818d43f67d3
andrei-stoica26 <andreistoica@foxmail.com>
2026-09-09 16:25:42 +0800

    Added sensAnalysis

--- sensitivity_analysis.R
+++ sensitivity_analysis.R
@@ -3,27 +3,45 @@ library(GSABenchmark)
 library(Seurat)
 library(qs2)
 
-seuratObj <- qs_read('seuratPanc.qs2')
-geneSets <- qs_read('geneSetsPanc.qs2')
-types <- names(geneSets)
-for (percVal in c(70, 75, 80, 85, 90, 95)){
-    markerSetNames <- paste0('CSOA', percVal, '_', types)
-    names(geneSets) <- markerSetNames
-    seuratObj <- runCSOA(seuratObj, geneSets, percentile=percVal)
+sensAnalysis <- function(seuratObj, geneSets, labelCol){
+    types <- names(geneSets)
+    for (percVal in c(70, 75, 80, 85, 90, 95)){
+        markerSetNames <- paste0('CSOA', percVal, '_', types)
+        names(geneSets) <- markerSetNames
+        seuratObj <- runCSOA(seuratObj, geneSets, percentile=percVal)
+    }
+    names(geneSets) <- types
+    smr <- runBenchmark(seuratObj,
+                        labelCol,
+                        geneSets,
+                        c('CSOA70',
+                          'CSOA75',
+                          'CSOA80',
+                          'CSOA85',
+                          'CSOA90',
+                          'CSOA95'),
+                        runEFBenchmark=FALSE)
+    return(smr)
 }
 
-names(geneSets) <- types
-smr <- runBenchmark(seuratObj,
-                    'label',
-                    geneSets,
-                    c('CSOA70',
-                      'CSOA75',
-                      'CSOA80',
-                      'CSOA85',
-                      'CSOA90',
-                      'CSOA95'))
+seuratObj <- qs_read('seuratPanc.qs2')
+geneSets <- qs_read('geneSetsPanc.qs2')
+labelCol <- 'label'
+smr <- sensAnalysis(seuratObj, geneSets, labelCol)
 
 View(smr$boundary$avg)
 View(smr$MCC$boundaryMCC)
 View(smr$MCC$directMCC)
 View(smr$global$avg)
+View(smr$efficiency$space)
+
+seuratObj <- qs_read('seuratLung.qs2')
+geneSets <- qs_read('geneSetsLung.qs2')
+labelCol <- 'celltype'
+smr <- sensAnalysis(seuratObj, geneSets, labelCol)
+
+View(smr$boundary$avg)
+View(smr$MCC$boundaryMCC)
+View(smr$MCC$directMCC)
+View(smr$global$avg)
+View(smr$efficiency$space)
